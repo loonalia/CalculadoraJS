37a42c2d6b81fd0921c8595e9c10888d66a3ded1
thaliags <thaliagsilva06@gmail.com>
2025-05-05 15:54:39 -0300

    finalizando JS

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const numero1Input = document.getElementById('numero1')
+const numero1Input = document.getElementById('numero1Input')
 const operacao = document.getElementById('operacao')
-const numero2Input = document.getElementById('numero2')
-const resultado = document.getElementById('resultado')
+const numero2Input = document.getElementById('numero2Input')
+const resultadoDiv = document.getElementById('resultadoDiv')
 
-function calcular () {
+function calcular (event) {
+    event.preventDefault()
     const numero1 = parseFloat(numero1Input.value)
     const numero2 = parseFloat(numero2Input.value)
     const operador = operacao.value
 
-    const calculo = [
-        {
-            "+": (a,b) => a + b
-        }, {
-            "-": (a,b) => a - b
-        }, {
-            "*": (a,b) => a * b
-        }, {
-            "/": (a,b) => a / b
+    const calculo = {
+            "+": (a,b) => a + b,
+            "-": (a,b) => a - b,
+            "*": (a,b) => a * b,
+            "/": (a,b) => {
+                if (b === 0) {
+                    return "Não é possível dividir por 0"
+                } else {
+                   return a / b
+                }
+            }
         }
-    ]
+
+    if (calculo[operador]) {
+        const resultado = calculo[operador](numero1, numero2)
+        resultadoDiv.innerHTML = `<p> Resultado: ${resultado} </p>`
+    } else {
+        resultadoDiv.innerHTML = '<p> Cálculo inválido </p>'
+    }
+    
+}
+
+function limparConteudo () {
+    numero1Input.value = ""
+    numero2Input.value = ""
+    operacao.value = "+"
+    resultadoDiv.innerHTML = ""
 }
